66ae7a5bff932e15af284be514617d7735029954
Sajid Sulaiman <sajedforpc@gmail.com>
2022-11-20 22:33:48 +0200

    date attr for clients

--- src/pages/Clients.jsx
+++ src/pages/Clients.jsx
@@ -1,17 +1,6 @@
 import DataTable from "@components/client/DataTable";
 
-import MagicSelect from "../components/ui/MagicSelect";
-import useLocalStorage from "@hooks/useLocalStorage";
-import { Col, Container, Form, Row } from "react-bootstrap";
-
-import { DEFAULT_CLIENT_STATUSES } from "@data/client";
-
 function Clients() {
-	const [statuses, setStatuses] = useLocalStorage(
-		"client-statuses",
-		DEFAULT_CLIENT_STATUSES
-	);
-
 	return (
 		<div>
 			<DataTable />
