6d73ebb946c0979b123a1c5803515a274c73f945
tubasi18 <mtn3213@gmail.com>
2022-11-23 23:10:08 +0200

    ClientDetails

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import { Dashboard, Clients, Reports } from "@pages";
+import { Dashboard, Clients, Reports, ClientDetails } from "@pages";
 import ClientsContext from "@contexts/ClientsContext";
 import Sidebar from "./components/client/Sidebar";
 import { Container } from "react-bootstrap";
 
 function App() {
-	return (
-		<div className="d-flex vh-100 overflow-hidden">
-			<Sidebar />
+  return (
+    <div className="d-flex vh-100 overflow-hidden">
+      <Sidebar />
 
-			<div className="flex-grow-1">
-				{/* The header goes here (position) */}
+      <div className="flex-grow-1">
+        {/* The header goes here (position) */}
 
-				<main className="page-layout">
-					<Container>
-						<ClientsContext>
-							<Routes>
-								{/* pages go here as Routes */}
-								<Route path="/" element={<Dashboard />} />
-								<Route path="/dashboard" element={<Dashboard />} />
-								<Route path="/clients" element={<Clients />} />
-								<Route path="/reports" element={<Reports />} />
-							</Routes>
-						</ClientsContext>
-					</Container>
-				</main>
-			</div>
-		</div>
-	);
+        <main className="page-layout">
+          <Container fluid>
+            <ClientsContext>
+              <Routes>
+                {/* pages go here as Routes */}
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/clients" element={<Clients />} />
+                <Route path="/reports" element={<Reports />} />
+                <Route path="/details/:id" element={<ClientDetails />} />
+              </Routes>
+            </ClientsContext>
+          </Container>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/ClientDetails.jsx
@@ -0,0 +1,32 @@
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "../components/Detailscss.css";
+import Button from "react-bootstrap/Button";
+import { Link, useParams } from "react-router-dom";
+
+const ClientDetails = () => {
+  const { id } = useParams();
+  return (
+    <div className="p-2 h-100">
+      <Button id="button-done" size="sm" as={Link} to="/clients">
+        Done
+      </Button>
+      <Row>
+        <Col xs={12} md={6} lg={4}>
+          <p>Client info</p>
+          <Button className="" size="sm">
+            Edit
+          </Button>
+        </Col>
+        <Col xs={12} md={6} lg={4} className="h-100">
+          {/* Put here ur component attachments */}
+        </Col>
+        <Col xs={12} md={4} lg={4} className="h-100">
+          {/* Put here ur component updates */}
+          {/* Put here ur component comments */}
+        </Col>
+      </Row>
+    </div>
+  );
+};
+export default ClientDetails;
