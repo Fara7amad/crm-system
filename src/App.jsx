e65e2fc321ede010e6f8460f569017258e5d7bdc
3bood.tala7m <3bood.tala7m@gmail.com>
2022-11-24 18:34:47 +0200

    data Bulling

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import { Dashboard, Clients, Reports } from "@pages";
 import ClientsContext from "@contexts/ClientsContext";
 import Sidebar from "./components/client/Sidebar";
 import { Container } from "react-bootstrap";
+import BullingStor from "./components/client/BullingStor";
 
 function App() {
 	return (
 		<div className="d-flex vh-100 overflow-hidden">
 			<Sidebar />
-
+			{/*<BullingStor/>*/}
 			<div className="flex-grow-1">
 				{/* The header goes here (position) */}
-
 				<main className="page-layout">
 					<Container>
 						<ClientsContext>
