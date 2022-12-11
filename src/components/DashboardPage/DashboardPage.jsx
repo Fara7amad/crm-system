3dfa7bf9939c0a40a538a23720d4c85635f09b5e
3bood.tala7m <3bood.tala7m@gmail.com>
2022-12-11 22:41:12 +0200

    dashboard but not well

--- src/components/DashboardPage/DashboardPage.jsx
+++ src/components/DashboardPage/DashboardPage.jsx
@@ -1,63 +1,163 @@
-import React, { useState } from "react";
-import alertPhoto from "./alertPhoto.png";
-import TextField from "@mui/material/TextField";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
 import "./DashboardPage.css";
-//npm install @mui/icons-material
-//npm install @mui/x-date-pickers
-//npm install date-fns
-//npm install @mui/material @emotion/react @emotion/styled
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faSackDollar } from "@fortawesome/free-solid-svg-icons";
+import { faUsers } from "@fortawesome/free-solid-svg-icons";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer , Legend ,  BarChart, Bar } from 'recharts';
+import React from 'react';
+
 
 const DashboardPage = () => {
-	const [value, setValue] = useState(new Date());
-	let newChart = 21;
-	let Report = 15;
-	let newMessage = 45;
-	let name = "SAM";
-	return (
-		<div>
-			<div className="container">
-				<img className="img" src={alertPhoto} />
+  
+  
 
-				<div className="alert main-alert" role="alert">
-					<h2 className="alert-heading">Welcome back {name}!</h2>
-					<b>since your last login on the system,there were :</b>
-					<br />
-					<b>{newChart}</b>
-					<a href="#" className="alert-link">
-						new charts
-					</a>
-					<br />
-					<b>{Report}</b>
-					<a href="#" className="alert-link">
-						reports
-					</a>
-					<br />
-					<b>{newMessage}</b>
-					<a href="#" className="alert-link">
-						new messages
-					</a>
-					<br />
-				</div>
-			</div>
+  let Total_customers = 500;
+  let Total_project  = 183;
+  let Ernings = 13840;
+ 
 
-			<div className="position-absolute bottom-0 end-0">
-				<LocalizationProvider dateAdapter={AdapterDateFns}>
-					<StaticDatePicker
-						variant="static"
-						orientation="portrait"
-						value={value}
-						disableFuture
-						onChange={(newValue) => setValue(newValue)}
-						renderInput={(params) => {
-							<TextField {...params} />;
-						}}
-					/>
-				</LocalizationProvider>
-			</div>
-		</div>
-	);
+  const data = [
+    { month: "Jan", project: 10 },
+    { month: "Feb", project: 20 },
+    { month: "Mar", project: 30 },
+    { month: "Ap",  project: 40 },
+    { month: "May", project: 50 },
+    { month: "Jun", project: 60 },
+    { month: "Jul", project: 70 },
+    { month: "Aug", project: 80 },
+    { month: "Sep", project: 90 },
+    { month: "Oc", project: 100 },
+    { month: "Nov", project: 110 },
+    { month: "Dec", project: 120 },
+  ];
+
+
+
+
+  const data2 = [
+    {
+      profit: 4000,
+      expenses: 2400,
+    },
+    {
+     
+      profit: 3000,
+      expenses: 3000,
+    },
+    {
+      profit: 2600,
+      expenses: 4400,
+    },
+    {
+      profit: 2500,
+      expenses: 1000,
+    },
+    {
+      profit: 2000,
+      expenses: 2000,
+    },
+    {
+      profit: 2000,
+      expenses: 2400,
+    },
+    {
+      profit: 3000,
+      expenses: 2700,
+    },
+  ];
+  
+  
+  return (
+    <>
+      <div className="container con1">
+        
+        <div className="row">
+         
+          <div className="col-3">
+            
+            <div className="card card-customers">
+            <i> <FontAwesomeIcon icon={faUser} className="iconn"/> <b>Total customers  {Total_customers} </b> </i> 
+            </div>
+
+          </div>
+      
+         
+          <div className="col-3">
+        
+            <div className="card card-projects">
+            <i> <FontAwesomeIcon icon={faUsers} className="iconn" /> <b>Total projects  {Total_project} </b> </i> 
+            </div>
+
+          </div>
+         
+         
+          <div className="col-3">
+          
+            <div className="card card-Ernings">
+            <i> <FontAwesomeIcon icon={faSackDollar} className="iconn" /> <b>Ernings {Ernings} </b>  </i> 
+            </div>
+
+          </div>
+
+          <div className="col-3">
+      
+            <div className="card card-last-login position-absolute top-1 end-0  ">
+            <b>since your Last log in : </b> <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+            </div>
+
+          </div>
+
+
+          <div className="col-3">
+      
+            <div className="card card-last-login  position-absolute bottom-0 end-0 ">
+            <b></b> <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+            </div>
+          </div>
+        
+        </div>
+      </div>
+    
+
+
+
+    <div className="container con2">
+
+
+      <div className="BarChart" >
+        <p></p>
+        <ResponsiveContainer width="74%" aspect={3}>
+        <BarChart  width={500} height={300} data={data} margin={{top: 15, right: 30, left: 20, bottom: 5, }} barSize={30} >
+        <XAxis dataKey="month" scale="point" padding={{ left: 15, right: 10 }} tick={{fill:"#black"}}/>
+        <YAxis tick={{fill:"#black"}} />
+        <Tooltip contentStyle={{ backgroundColor: "#8884d8", color: "#fff" }} itemStyle={{ color: "#fff" }} cursor={false} />
+        <Legend />
+        <CartesianGrid horizontal="true" vertical="" stroke="#243240" opacity={0.0} />
+        <Bar dataKey="project" fill="#632cba" background={{ fill: "#8884d8" }} />
+        </BarChart> 
+        </ResponsiveContainer>
+      </div>
+
+      <div className="LineChart">
+        <p></p>
+        <ResponsiveContainer width="74%" aspect={3}>
+        <LineChart width={500} height={300} data={data2} margin={{top: 15, right: 30, left: 20, bottom: 5, }}>
+        <CartesianGrid  horizontal="true" vertical="" stroke="#243240" opacity={0.0}/>
+        <XAxis dataKey="profit" tick={{fill:"#black"}}/>
+        <YAxis tick={{fill:"black"}} />
+        <Tooltip contentStyle={{ backgroundColor: "#8884d8", color: "#fff" }} itemStyle={{ color: "#fff" }} cursor={false}/>
+        <Legend />
+        <Line type="monotone" dataKey="profit" stroke="#8884d8" strokeWidth="5" dot={{fill:"#2e4355",stroke:"#8884d8",strokeWidth: 2,r:5}} activeDot={{fill:"#2e4355",stroke:"#8884d8",strokeWidth: 5,r:10}} />
+        <Line type="monotone" dataKey="expenses" stroke="#632cba" strokeWidth="5" dot={{fill:"#2e4355",stroke:"#8884d8",strokeWidth: 2,r:5}} activeDot={{fill:"#2e4355",stroke:"#8884d8",strokeWidth: 5,r:10}} />
+        </LineChart>
+        </ResponsiveContainer>
+      </div>
+
+   
+
+
+    </div>
+    </>
+  );
 };
 export default DashboardPage;
--- src/components/DashboardPage/NavigationBar.jsx
+++ src/components/DashboardPage/NavigationBar.jsx
@@ -1,32 +1,34 @@
-import "./NavigationBar.css";
+import './NavigationBar.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function NavigationBar() {
-	return (
-		<div className="nav navbar">
-			<div className="notification"></div>
-			<div className="exsit-notification">
-				<button type="button" className="icon-button">
-					<i className="material-icons bi-bell "></i>
-					<span className="icon-button__badge"></span>
-				</button>
+    return (
+        <div className="nav navbar">
+        <div className="notification"></div>
+        
+        <div className="exsit-notification">
+          <button type="button" className="icon-button">
+            <i className="material-icons bi-bell "> <FontAwesomeIcon icon="fa-regular fa-bell" /> </i>
+            <span className="icon-button__badge"></span>
+          </button>
 
-				<button type="button" className="icon-button">
-					<i className="bi-exit bi-box-arrow-right"></i>
-				</button>
-			</div>
+          <button type="button" className="icon-button">
+            <i className="bi-exit bi-box-arrow-right"> <FontAwesomeIcon icon="fa-regular fa-arrow-right-from-bracket" /> </i>
+          </button>
+        </div>
 
-			<div className="search-box">
-				<input
-					className="search-txt"
-					type="text"
-					name=""
-					placeholder="Type to search"
-				></input>
-				<a className="search-btn" href="#">
-					{" "}
-					<i className="fas fa-search"></i>
-				</a>
-			</div>
-		</div>
-	);
+        <div className="search-box">
+          <input
+            className="search-txt"
+            type="text"
+            name=""
+            placeholder="Type to search"
+          ></input>
+          <a className="search-btn" href="#">
+            {" "}
+            <i className="fas fa-search"> <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" /> </i>
+          </a>
+        </div>
+      </div>
+    ) 
 }
