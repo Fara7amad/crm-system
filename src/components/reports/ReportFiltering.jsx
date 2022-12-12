f104d10dd921c0d553e0955f6cb916e86bb2ec6b
Muhammad-Qzih <a90235145+1@gmail.com>
2022-12-12 21:41:18 +0200

    reports filtering

---
+++ src/components/reports/ReportFiltering.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import { useClient } from "@contexts/ClientsContext";
+import Form from "react-bootstrap/Form";
+import Dropdown from "react-bootstrap/Dropdown";
+import "../reports/ReportFiltering.css";
+import Button from "react-bootstrap/Button";
+import Stack from "react-bootstrap/Stack";
+
+const ReportFiltering = () => {
+  //---------------------------Searching-------------------------------------
+
+  const clients = useClient();
+  const [filteredList, setFilteredList] = useState(clients);
+
+  useEffect(() => {
+    setFilteredList(clients);
+  }, [clients]);
+
+  const [search, setSearch] = useState("");
+  const searchData = (e) => {
+    if (e.target.value.trim() != "") {
+      setFilteredList(
+        clients.filter((user) =>
+          user.company
+            .toLowerCase()
+            .includes(e.target.value.trim().toLowerCase())
+        )
+      );
+    }
+    setSearch(e.target.value);
+  };
+  //----------------------------------------------------------------asdasd
+
+  return (
+    <>
+      <form action="">
+        <Stack direction="horizontal" gap={3}>
+          <Form.Control
+            type="text"
+            id="search-barReport"
+            placeholder="Search"
+            className="search-bar"
+            value={search}
+            onChange={searchData}
+          />
+          <br />
+          <Stack direction="horizontal" gap={3}></Stack>
+        </Stack>{" "}
+      </form>
+    </>
+  );
+};
+
+export default ReportFiltering;
--- src/pages/Reports.jsx
+++ src/pages/Reports.jsx
@@ -1,21 +1,22 @@
 import { Col, Row } from "react-bootstrap";
-import PeriodChart from "../components/report/PeriodChart";
-import StatusChart from "../components/report/StatusChart";
+import ReportFiltering from "../components/reports/reportFiltering";
 
 function Reports() {
-	return (
-		<div>
-			<h1 className="mb-4">Reports</h1>
+  return (
+    <div>
+      <h1 className="mb-4">Reports</h1>
 
-			<Row>
-				<Col xs={12} md={6}>
-					<PeriodChart />
-				</Col>
-				<Col xs={12} md={6}>
-					<StatusChart />
-				</Col>
-			</Row>
-		</div>
-	);
+      <ReportFiltering />
+
+      {/* <Row>
+        <Col xs={12} md={6}></Col>
+        <Col xs={12} md={6}>
+         
+        
+        </Col>
+        <Col xs={12} md={6}></Col>
+      </Row> */}
+    </div>
+  );
 }
 export default Reports;
