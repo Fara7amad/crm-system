6f48afa40df50f0f74fdb30dc51befe49c035b75
3bood.tala7m <3bood.tala7m@gmail.com>
2022-12-20 21:12:10 +0200

    test

--- src/components/dashboard/LineChart.jsx
+++ src/components/dashboard/LineChart.jsx
@@ -2,30 +2,12 @@ import React from "react";
 import Chart from "chart.js/auto";
 import { Line } from "react-chartjs-2";
 
-const labels = ["2020", "2021", "2022"];
 
-const data = {
-  labels: labels,
-  datasets: [
-    {
-      label: "Profits in thousands",
-      backgroundColor: "rgb(255, 99, 132)",
-      borderColor: "rgb(255, 99, 132)",
-      data: [4,11,27],
-    },
-    {
-        label: "Expensess in thousands",
-      backgroundColor: "rgb(182, 99, 255)",
-      borderColor: "rgb(182, 99, 255)",
-      data: [3.5,3,7],
-    }
-  ],
-};
 
 const LineChart = () => {
   return (
     <div>
-      <Line data={data} />
+
     </div>
   );
 };
