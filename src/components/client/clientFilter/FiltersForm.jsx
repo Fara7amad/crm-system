d50aa47acda03aa1199d0a54351f54c58477297e
oealpha-admin <oealpha@atlassian.net>
2022-12-24 18:44:35 +0200

    Adding changes of reports, filtering by searching and other stuff

--- src/components/client/clientFilter/FiltersForm.jsx
+++ src/components/client/clientFilter/FiltersForm.jsx
@@ -5,92 +5,126 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Dropdown from "react-bootstrap/Dropdown";
 import { Stack } from "react-bootstrap";
-
-const defaultFilters = { state: "", type: "" };
+import useLocalStorage from "@hooks/useLocalStorage";
+import { DEFAULT_CLIENT_STATUSES } from "@data/client";
+const defaultFilters = { state: "", type: "", status: "" };
 
 const FiltersForm = ({ filteredList, setFilterList }) => {
-	const [filters, setFilters] = useState(defaultFilters);
-	const clients = useClient();
+  const [filters, setFilters] = useState(defaultFilters);
+  const clients = useClient();
+  console.log(clients);
+  const allStateClients = [...new Set(clients.map((client) => client.state))];
 
-	const allStateClients = [...new Set(clients.map((client) => client.state))];
+  const [statuses] = useLocalStorage(
+    "client-statuses", //key in local storage
+    DEFAULT_CLIENT_STATUSES
+  );
 
-	const onChangeState = (e) => {
-		setFilters((prev) => {
-			return { ...prev, state: e.target.value };
-		});
-	};
+  const onChangeState = (e) => {
+    setFilters((prev) => {
+      return { ...prev, state: e.target.value };
+    });
+  };
 
-	const onChangeType = (e) => {
-		setFilters((prev) => {
-			return { ...prev, type: e.target.value };
-		});
-	};
+  const onChangeType = (e) => {
+    setFilters((prev) => {
+      return { ...prev, type: e.target.value };
+    });
+  };
+  const onChangeStatus = (e) => {
+    console.log(e.target.value);
+    setFilters((prev) => {
+      return { ...prev, status: e.target.value };
+    });
+  };
+  const onClickAll = () => {
+    setFilterList(clients);
+    setFilters(defaultFilters);
+  };
 
-	const onClickAll = () => {
-		setFilterList(clients);
-		setFilters(defaultFilters);
-	};
+  const onSubmit = (event) => {
+    event.preventDefault();
 
-	const onSubmit = (event) => {
-		event.preventDefault();
+    let filtered = [...clients];
 
-		let filtered = [...clients];
+    if (filters.type)
+      filtered = [...filtered.filter((client) => client.type == filters.type)];
 
-		if (filters.type)
-			filtered = [...filtered.filter((client) => client.type == filters.type)];
+    if (filters.state)
+      filtered = [
+        ...filtered.filter((client) => client.state == filters.state),
+      ];
 
-		if (filters.state)
-			filtered = [
-				...filtered.filter((client) => client.state == filters.state),
-			];
+    if (filters.status)
+      filtered = [
+        ...filtered.filter((client) => client.status == filters.status),
+      ];
 
-		setFilterList(filtered);
-	};
+    setFilterList(filtered);
+  };
 
-	return (
-		<div>
-			<Dropdown>
-				<Dropdown.Toggle variant="primary">Filter</Dropdown.Toggle>
+  return (
+    <div>
+      <Dropdown>
+        <Dropdown.Toggle variant="primary">Filter</Dropdown.Toggle>
 
-				<Dropdown.Menu className="form">
-					<form onSubmit={onSubmit}>
-						<Form.Label htmlFor="type">Type</Form.Label>
-						<Form.Select value={filters.type} onChange={onChangeType}>
-							<option value="">Type</option>
-							<option value="Customer">Customer</option>
-							<option value="Lead">Lead</option>
-						</Form.Select>
+        <Dropdown.Menu className="form">
+          <form onSubmit={onSubmit}>
+            <Form.Label htmlFor="type">Type</Form.Label>
+            <Form.Select value={filters.type} onChange={onChangeType}>
+              <option value="">Type</option>
+              <option value="Customer">Customer</option>
+              <option value="Lead">Lead</option>
+            </Form.Select>
 
-						<br />
+            <br />
 
-						<Form.Label htmlFor="state">State</Form.Label>
-						<Form.Select value={filters.state} onChange={onChangeState}>
-							<option value="">State</option>
-							{allStateClients.map((state) => {
-								return <option>{state}</option>;
-							})}
-						</Form.Select>
+            <Form.Label htmlFor="state">State</Form.Label>
+            <Form.Select value={filters.state} onChange={onChangeState}>
+              <option value="">State</option>
+              {allStateClients.map((state) => {
+                return <option>{state}</option>;
+              })}
+            </Form.Select>
+            <br />
+            <Form.Label htmlFor="status">ٍStatus</Form.Label>
+            <Form.Select
+              value={filters.status}
+              id="status"
+              onChange={onChangeStatus}
+            >
+              <option value="">Status</option>
+              {statuses.map((status) => {
+                return (
+                  <option
+                    value={status.toLowerCase()}
+                    className="text-capitalize"
+                  >
+                    {status.split("-").join(" ")}
+                  </option>
+                );
+              })}
+            </Form.Select>
+            <br />
 
-						<br />
+            <Stack direction="horizontal" gap={1}>
+              <Button type="submit" className="btn-submit">
+                Submit
+              </Button>
 
-						<Stack direction="horizontal" gap={1}>
-							<Button type="submit" className="btn-submit">
-								Submit
-							</Button>
-
-							<Button
-								type="button"
-								onClick={onClickAll}
-								variant="outline-danger"
-							>
-								Reset
-							</Button>
-						</Stack>
-					</form>
-				</Dropdown.Menu>
-			</Dropdown>
-		</div>
-	);
+              <Button
+                type="button"
+                onClick={onClickAll}
+                variant="outline-danger"
+              >
+                Reset
+              </Button>
+            </Stack>
+          </form>
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
+  );
 };
 
 export default FiltersForm;
--- src/pages/Reports.jsx
+++ src/pages/Reports.jsx
@@ -8,96 +8,98 @@ import ReactPaginate from "react-paginate";
 import ReportFiltering from "@components/reports/ReportFiltering";
 
 function Reports() {
-	const clients = useClient();
-	const [filteredList, setFilteredList] = useState(clients);
-	const [sortType, setSortType] = useState("asc");
+  const clients = useClient();
+  const [filteredList, setFilteredList] = useState(clients);
+  const [sortType, setSortType] = useState("asc");
 
-	const { handlePageClick, currentItems, pageCount, classes } = usePagination(
-		filteredList,
-		15
-	);
+  const { handlePageClick, currentItems, pageCount, classes } = usePagination(
+    filteredList,
+    15
+  );
 
-	const handleSortColumn = (key, sortType) => {
-		if (key != "date")
-			setFilteredList((prev) => [
-				...prev.sort((a, b) =>
-					sortType == "asc"
-						? a[key].localeCompare(b[key])
-						: b[key].localeCompare(a[key])
-				),
-			]);
-		else
-			setFilteredList((prev) => [
-				...prev.sort((a, b) =>
-					sortType == "asc" ? a[key] - b[key] : b[key] - a[key]
-				),
-			]);
-		setSortType(sortType == "asc" ? "desc" : "asc");
-	};
+  console.log(clients);
+  const handleSortColumn = (key, sortType) => {
+    if (key != "date")
+      setFilteredList((prev) => [
+        ...prev.sort((a, b) =>
+          sortType == "asc"
+            ? a[key].localeCompare(b[key])
+            : b[key].localeCompare(a[key])
+        ),
+      ]);
+    else
+      setFilteredList((prev) => [
+        ...prev.sort((a, b) =>
+          sortType == "asc" ? a[key] - b[key] : b[key] - a[key]
+        ),
+      ]);
+    setSortType(sortType == "asc" ? "desc" : "asc");
+  };
 
-	return (
-		<div>
-			<ReportFiltering
-				setFilteredList={setFilteredList}
-				filteredList={filteredList}
-			/>
+  return (
+    <div>
+      <ReportFiltering
+        setFilteredList={setFilteredList}
+        filteredList={filteredList}
+      />
 
-			<Table
-				data={currentItems}
-				sortType={sortType}
-				onSortColumn={handleSortColumn}
-				autoHeight
-				virtualized
-			>
-				<Column>
-					<HeaderCell>ID</HeaderCell>
-					<Cell
-						dataKey="id"
-						renderCell={(id) =>
-							filteredList.findIndex((client) => client.id === id) + 1
-						}
-					/>
-				</Column>
-				<Column sortable flexGrow={1} minWidth={200}>
-					<HeaderCell>Company</HeaderCell>
-					<Cell dataKey="company" />
-				</Column>
-				<Column sortable width={200}>
-					<HeaderCell>First Name</HeaderCell>
-					<Cell dataKey="firstName" />
-				</Column>
-				<Column sortable width={200}>
-					<HeaderCell>Last Name</HeaderCell>
-					<Cell dataKey="lastName" />
-				</Column>
-				<Column sortable width={140}>
-					<HeaderCell>Date Added</HeaderCell>
-					<Cell dataKey="date" renderCell={(data) => formatDate(data)} />
-				</Column>
-				<Column flexGrow={1} minWidth={100}>
-					<HeaderCell>Type</HeaderCell>
-					<Cell dataKey="type" />
-				</Column>
-				<Column flexGrow={1} minWidth={130}>
-					<HeaderCell>Status</HeaderCell>
-					<Cell
-						className="text-capitalize"
-						dataKey="status"
-						renderCell={(status) => status.split("-").join(" ")}
-					/>
-				</Column>
-			</Table>
+      <Table
+        data={currentItems}
+        sortType={sortType}
+        onSortColumn={handleSortColumn}
+        autoHeight
+        virtualized
+      >
+        <Column>
+          <HeaderCell>ID</HeaderCell>
+          <Cell
+            dataKey="id"
+            renderCell={(id) =>
+              filteredList.findIndex((client) => client.id === id) + 1
+            }
+          />
+        </Column>
 
-			<div className="mt-2">
-				<ReactPaginate
-					onPageChange={handlePageClick}
-					pageRangeDisplayed={5}
-					pageCount={pageCount}
-					{...classes}
-				/>
-			</div>
-		</div>
-	);
+        <Column sortable flexGrow={1} minWidth={200}>
+          <HeaderCell>Company</HeaderCell>
+          <Cell dataKey="company" />
+        </Column>
+        <Column sortable width={200}>
+          <HeaderCell>First Name</HeaderCell>
+          <Cell dataKey="firstName" />
+        </Column>
+        <Column sortable width={200}>
+          <HeaderCell>Last Name</HeaderCell>
+          <Cell dataKey="lastName" />
+        </Column>
+        <Column sortable width={140}>
+          <HeaderCell>Date Added</HeaderCell>
+          <Cell dataKey="date" renderCell={(data) => formatDate(data)} />
+        </Column>
+        <Column flexGrow={1} minWidth={100}>
+          <HeaderCell>Type</HeaderCell>
+          <Cell dataKey="type" />
+        </Column>
+        <Column flexGrow={1} minWidth={130}>
+          <HeaderCell>Status</HeaderCell>
+          <Cell
+            className="text-capitalize"
+            dataKey="status"
+            renderCell={(status) => status.split("-").join(" ")}
+          />
+        </Column>
+      </Table>
+
+      <div className="mt-2">
+        <ReactPaginate
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          {...classes}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Reports;
